918adbfa2090c072cb44859b590ca2067ee59073
Higor V.L <higor-lopo@hotmail.com>
2023-05-30 21:22:34 -0400

    Implementada verificação de campos e ativação do botão

--- app/src/main/java/com/example/jogodaforca/MainActivity.java
+++ app/src/main/java/com/example/jogodaforca/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -18,7 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton btchar1, btchar2, btchar3, btchar4;
-    private TextView tvNome;
+    private EditText nick;
     private List<ImageButton> imageButtons;
     private boolean isClicked = false;
     private Button Entrar;
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Entrar = (Button) findViewById(R.id.btEntrar);
         Entrar.setEnabled(false);
 
-        tvNome = (TextView) findViewById(R.id.tvName);
+        nick = (EditText) findViewById(R.id.etNick);
         btchar1 = (ImageButton) findViewById(R.id.btchar1);
         btchar2 = (ImageButton) findViewById(R.id.btChar2);
         btchar3 = (ImageButton) findViewById(R.id.btChar3);
@@ -48,40 +51,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (ImageButton imageButton : imageButtons) {
             imageButton.setOnClickListener(this);
         }
+
+        nick.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Não é necessário implementar esse método
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Não é necessário implementar esse método
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                updateEntrarButtonState();
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         for (ImageButton imageButton : imageButtons) {
             if (imageButton.getId() == v.getId()) {
-                // Botão clicado
                 isClicked=true;
                 imageButton.setSelected(true);
                 ImageViewCompat.setImageTintList(imageButton, null); // Remover a cor
-
+                updateEntrarButtonState();
             } else {
-                // Outros botões
                 imageButton.setSelected(false);
                 ImageViewCompat.setImageTintList(imageButton, ContextCompat.getColorStateList(this, R.color.grey)); // Aplicar cor azul
             }
         }
-
-        validar();
     }
 
-    private void validar(){
-        if(tvNome.getText().toString().equals("a")){
-            Entrar.setEnabled(true);
+    private void updateEntrarButtonState() {
+        boolean isNickEmpty = nick.getText().toString().isEmpty();
+        boolean isAnyImageButtonSelected = btchar1.isSelected() || btchar2.isSelected() || btchar3.isSelected() || btchar4.isSelected();
+        Entrar.setEnabled(!isNickEmpty && isAnyImageButtonSelected);
+        if (Entrar.isEnabled()) {
+            Entrar.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.green));
+            Entrar.setTextColor(Color.WHITE);
+        } else {
+            Entrar.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.grey));
+            Entrar.setTextColor(Color.BLACK);
         }
     }
 
     public void entrar(View v){
         System.out.println("faz alguma coisa");
     }
-
-
-
-
-
 
 }
