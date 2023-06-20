1024900e3f128dc8671c1843f0131963b2f0a8c6
jose-115 <joseveiga1234@gmail.com>
2023-06-19 21:03:03 -0400

    Criação da tela de jogo

--- app/src/main/java/com/example/jogodaforca/MenuDificuldade.java
+++ app/src/main/java/com/example/jogodaforca/MenuDificuldade.java
@@ -1,16 +1,64 @@
 package com.example.jogodaforca;
 
+import static android.widget.Button.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuDificuldade extends AppCompatActivity {
 
+    private List<Button> Buttons;
+    TextView tv;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_dificuldade);
         getSupportActionBar().hide();
 
+        tv = findViewById(R.id.tvTI);
+        Button button1 = findViewById(R.id.btFacil); // Substitua "button1" pelo ID do primeiro botão
+        Button button2 = findViewById(R.id.btMedio); // Substitua "button2" pelo ID do segundo botão
+        Button button3 = findViewById(R.id.btDificil); // Substitua "button3" pelo ID do terceiro botão
+
+        // Adicione o listener de clique aos botões
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                String x = button.getTag().toString();
+                Jogar(x);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                String x = button.getTag().toString();
+                Jogar(x);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                String x = button.getTag().toString();
+                Jogar(x);
+            }
+        });
+    }
+
+    public void Jogar(String x) {
+        tv.setText("Botão selecionado: " + x);
     }
 }
---
+++ app/src/main/java/com/example/jogodaforca/TelaJogo.java
@@ -0,0 +1,55 @@
+package com.example.jogodaforca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class TelaJogo extends AppCompatActivity {
+
+    Resources resources = getResources();
+    TypedArray animaisArray = resources.obtainTypedArray(R.array.animais);
+    public User user;
+
+    private TextView nick;
+    private ImageView avatar;
+
+    // Gera um índice aleatório
+    Random random = new Random();
+    int indiceAleatorio = random.nextInt(animaisArray.length());
+
+    // Obtém a palavra aleatória
+    int resourceId = animaisArray.getResourceId(indiceAleatorio, 0);
+    String palavraAleatoria = resources.getString(resourceId);
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_jogo);
+
+        nick = (TextView) findViewById(R.id.tvJogador);
+        avatar = (ImageView) findViewById(R.id.ivJogador);
+        preencherCampos();
+    }
+
+    private void preencherCampos() {
+        Intent intent = getIntent();
+        if (intent.hasExtra("user")){
+            User user;
+            user = (User) intent.getSerializableExtra("user");
+            nick.setText(user.getNick());
+
+            int idImagem = getResources().getIdentifier(user.getFoto(), "drawable", getPackageName());
+            avatar.setImageResource(idImagem);
+        }
+    }
+}
